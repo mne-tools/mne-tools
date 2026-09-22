60427fe9158207965401e19440701736eb72a17b
Thomas S. Binns <t.s.binns@outlook.com>
2026-09-22 20:08:41 +0100

    Add name mapping to requirement overrides

--- src/mne_tools/sync_dependencies_to_environment_file.py
+++ src/mne_tools/sync_dependencies_to_environment_file.py
@@ -2,12 +2,13 @@
 # License: BSD-3-Clause
 # Copyright the MNE-Tools contributors.
 
+import logging
 from argparse import SUPPRESS, ArgumentParser
 from pathlib import Path
-from packaging.requirements import Requirement
-import logging
 
-from mne_tools.helpers import split_optional_args, read_pyproject, PIP_CONDA_MAPPING
+from packaging.requirements import Requirement, SpecifierSet
+
+from mne_tools.helpers import read_pyproject, split_optional_args
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,7 +29,7 @@ def main():
     parser.add_argument(
         "--extras",
         type=str,
-        default="",
+        default=None,
         help=(
             "Comma-separated names of extra dependencies in `pyproject.toml`'s "
             "`[project.optional-dependencies]` that should be included in "
@@ -38,7 +39,7 @@ def main():
     parser.add_argument(
         "--additional-dependencies",
         type=str,
-        default="",
+        default=None,
         help=(
             "Comma-separated names of additional dependencies that are not included in "
             "`pyproject.toml`, but which should be included in `environment.yml`."
@@ -53,7 +54,7 @@ def main():
     parser.add_argument(
         "--pip-dependencies",
         type=str,
-        default="",
+        default=None,
         help=(
             "Comma-separated names of dependencies which should be installed via pip. "
             "The should be dependencies already included as core dependencies, in "
@@ -64,11 +65,17 @@ def main():
     parser.add_argument(
         "--requirements-overrides",
         type=str,
-        default="",
+        default=None,
         help=(
             "Comma-separated dependencies parseable by "
             "`packaging.requirements.Requirement`, which will override the information "
-            "for those same dependencies as specified in `pyproject.toml`."
+            "for those same dependencies as specified in `pyproject.toml`. "
+            "Pip-to-conda name mapping can be provided using `->`, e.g., "
+            "`mne->mne-base` will add `mne-base` instead of `mne` to "
+            "`environment.yml`. Existing requirements will be carried over, unless "
+            "overridden explicitly, e.g., if `mne>=1.6` is the dependency in "
+            "`pyproject.toml`, `mne->mne-base>=1.10` will put `mne-base>=1.10` in "
+            "`environment.yml`."
         ),
     )
 
@@ -83,8 +90,6 @@ def main():
         raise ValueError("At least one channel must be specified in `channels`.")
     pip_dependencies = split_optional_args(args.pip_dependencies)
     requirements_overrides = split_optional_args(args.requirements_overrides)
-    requirements_overrides = [Requirement(req) for req in requirements_overrides]
-    requirements_overrides = {req.name: req for req in requirements_overrides}
 
     # Get the `pyproject.toml` dependency info
     pyproject = read_pyproject(project_root=project_root)
@@ -96,16 +101,36 @@ def main():
     # Remove recursive dependencies
     deps = [dep for dep in deps if dep.name != pyproject["project"]["name"]]
 
+    # Parse requirement overrides
+    overrides = dict()
+    dep_names = [dep.name for dep in deps]
+    for req in requirements_overrides:
+        if "->" in req:  # handle requirement renaming
+            split_reqs = req.split("->")  # expected form is `pyproj_name->env_req`
+            if len(split_reqs) != 2:
+                raise ValueError(
+                    f"Invalid requirement override with name mapping: {req}. Expected "
+                    "format is '<pyproject.toml name> -> <environment.yml name>'."
+                )
+            pyproj_req, env_req = Requirement(split_reqs[0]), Requirement(split_reqs[1])
+            _check_pyproj_dep_present(pyproj_req.name, dep_names)
+            pyproj_dep = deps[dep_names.index(pyproj_req.name)]
+            # Carry over specifiers/extras/markers if not overridden
+            for attr, empty in zip(
+                ["specifier", "extras", "marker"], [SpecifierSet(), dict(), None]
+            ):
+                if getattr(env_req, attr) == empty and getattr(pyproj_dep, attr):
+                    setattr(env_req, attr, getattr(pyproj_dep, attr))
+            overrides[pyproj_req.name] = env_req
+        else:  # else use override directly
+            req = Requirement(req)
+            _check_pyproj_dep_present(req.name, dep_names)
+            overrides[req.name] = req
+
     # Override dependency info
-    for dep in requirements_overrides.keys():
-        if dep not in [d.name for d in deps]:
-            raise ValueError(
-                f"Dependency {dep} in `requirements_overrides` is not specified in "
-                "`pyproject.toml`."
-            )
     for idx, dep in enumerate(deps):
-        if dep.name in requirements_overrides:
-            deps[idx] = requirements_overrides[dep.name]
+        if dep.name in overrides:
+            deps[idx] = overrides[dep.name]
 
     # Add additional dependencies
     deps.extend(Requirement(dep) for dep in additional_dependencies)
@@ -136,19 +161,6 @@ def main():
             pip_deps[name] = req
     conda_deps = {name: req for name, req in deps.items() if name not in pip_deps}
 
-    # Map pip names to conda names
-    mapped_reqs = {}
-    for name, req in conda_deps.items():
-        new_name = PIP_CONDA_MAPPING.get(name, name)
-        if new_name == name:
-            continue
-        new_req = Requirement(str(req).replace(name, new_name, 1))
-        mapped_reqs[name] = new_req
-    conda_deps = {
-        name: req for name, req in conda_deps.items() if name not in mapped_reqs
-    }
-    conda_deps.update({req.name: req for req in mapped_reqs.values()})
-
     # Construct the `environment.yml` content
     env_contents = f"""\
 # THIS FILE IS AUTO-GENERATED BY MNE-TOOLS AND WILL BE OVERWRITTEN
@@ -176,6 +188,14 @@ dependencies:
         Path(project_root) / "environment.yml", "w", encoding="utf-8"
     ) as env_file:
         env_file.write(env_contents)
+
+
+def _check_pyproj_dep_present(check: str, deps: list[str]) -> None:
+    if check not in deps:
+        raise ValueError(
+            f"Dependency '{check}' in `requirements_overrides` is not specified in "
+            "`pyproject.toml`."
+        )
 
 
 if __name__ == "__main__":
